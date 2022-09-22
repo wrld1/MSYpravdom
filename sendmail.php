20a97289b890b58f511f4174b0aa629aee114ab0
Kyrylo Lolenko <98611618+wrld1@users.noreply.github.com>
2022-09-22 19:27:12 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -12,8 +12,8 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();                               
 $mail->Host       = 'smtp.gmail.com';                 
 $mail->SMTPAuth   = true;                            
-$mail->Username   = 'kir260302@gmail.com';   
-$mail->Password = 'yrsrtxcanbdddwqh';                           
+$mail->Username   = '';   
+$mail->Password = '';                           
 $mail->SMTPSecure = 'tls';  
 $mail->Port       = '587';    
 
